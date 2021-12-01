7a41be30ade6153ad54fc69d719effef230d508d
Justb612 <borisz0929@gmail.com>
2021-12-01 19:28:25 +0100

    deploy

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import Header from "./Components/Header";
+import Customize from "./Components/Customize";
+import Checkout from "./Components/Checkout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [ingredients, setIngredients] = useState({
+    basil: false,
+    cheese: false,
+    mushroom: false,
+    olive: false,
+    pineapple: false,
+    tomato: false,
+  });
+
+  useEffect(() => {
+    const data = localStorage.getItem("ingredients");
+    if (data) {
+      setIngredients(JSON.parse(data));
+    }
+  }, []);
+
+  return (
+    <div>
+      <Header />
+
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/checkout"
+            element={<Checkout ingredients={ingredients} />}
+          />
+          <Route
+            path="/"
+            element={
+              <Customize
+                ingredients={ingredients}
+                setIngredients={setIngredients}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
